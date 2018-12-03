562f1ae4542a9b35e0bdc46e4451bf055bd25a09
symonline <loshoke@apelasset.com>
2018-12-03 02:40:19 +0100

    ok

--- willapp/forms.py
+++ willapp/forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired
 
 ''' Building the sign in form class '''
-class LoginForm(FlaskForm):
+class LoginForm( FlaskForm ):
 
     username = StringField('Username',validators=[DataRequired()])
     password = PasswordField('Password',validators=[DataRequired()])
--- willapp/routes.py
+++ willapp/routes.py
@@ -1,12 +1,12 @@
-from flask import render_template,url_for
+from flask import render_template, flash, redirect, url_for
 from willapp import app
 from willapp.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user={'username':'David'}
-    posts=[
+    user = {'username':'David'}
+    posts = [
         {'author':{'username':'David'},
          'body':'What a beautiful weekend'
             },
@@ -15,9 +15,13 @@ def index():
          'body': 'Mission accomplished over the weekend'
             }
     ]
-    return render_template('index.html',title='',user=user,posts=posts)
+    return render_template('index.html', title='', user=user, posts=posts)
 
-@app.route('/login')
+@app.route('/login', methods = ['GET', 'POST'])
 def login():
     form = LoginForm()
-    return render_template('login.html',title='Sign In',form=form)
+    if form.validate_on_submit():
+        flash('Login requested for user {}, Remember_me={}'.format(\
+              form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
