cb4d52093a10a2bd136970b6084e03e787bb0101
symonline <loshoke@apelasset.com>
2018-12-04 00:12:04 +0100

    ok

--- config.py
+++ config.py
@@ -1,4 +1,15 @@
 import os
+basedir=os.path.abspath(os.path.dirname(__file__))
+
 class Config(object):
+
+    # retrieve secrete key from environment variable or default
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-not-die'
+
+    # retrieve database source from environment variable or default or file
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+            'sqlite:///' + os.path.join(basedir, 'app.db')
+
+    # used to signal/notify the application when an object changes
+    SQLALCHEMY_TRACK_MODIFICATION = False
 
